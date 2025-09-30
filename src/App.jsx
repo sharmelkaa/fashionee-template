e7d75e5b343e3b443f76a6e77b45af25fb06c80e
yuliya.prokofieva <yuliya.prokofieva@ladadigit.io>
2025-09-30 12:12:34 +0500

    Переключение страниц

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {useState} from 'react';
+
 function App() {
+  const [page, setPage] = useState('shop')
+
   return (
     <div>
-      Место для реализации FASHIONEE. Удачи!
+      {page === 'shop' && <h1 data-testid="shop-page">Shop</h1>}
+      {page === 'cart' && <h1 data-testid="cart-page">Cart</h1>}
+      <button onClick={() => setPage('shop')} data-testid="shop-btn">Shop</button>
+      <button onClick={() => setPage('cart')} data-testid="cart-btn">Cart</button>
     </div>
   )
 }
