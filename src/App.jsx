3d4a5fda9df94203a5493910024b01d03bd3d40a
yuliya.prokofieva <yuliya.prokofieva@ladadigit.io>
2025-09-30 12:32:20 +0500

    для тестов

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,22 @@ function App() {
 
   return (
     <div>
-      {page === 'shop' && <h1 data-testid="shop-page">Shop</h1>}
+      <header data-testid="header">Header</header>
+      {page === 'shop' && <div data-testid="content-block">
+        <h1 data-testid="shop-page">Shop</h1>
+        <ul data-testid="showcase">
+          <li data-testid="product-card">Item 1</li>
+          <li data-testid="product-card">Item 2</li>
+          <li data-testid="product-card">Item 3</li>
+        </ul>
+      </div>}
+
       {page === 'cart' && <h1 data-testid="cart-page">Cart</h1>}
+
       <button onClick={() => setPage('shop')} data-testid="shop-btn">Shop</button>
       <button onClick={() => setPage('cart')} data-testid="cart-btn">Cart</button>
+
+      <footer data-testid="footer">Footer</footer>
     </div>
   )
 }
